fde4694a63de0356d1f7cbdb061f18aeac54d16f
mmewery <mmewery@gmail.com>
2026-05-14 22:32:51 +0200

    Fixed light intensity

--- src/winterlandscape/SceneLighting.java
+++ src/winterlandscape/SceneLighting.java
@@ -24,15 +24,25 @@ public class SceneLighting {
     public void apply() {
         float[] pos = {(float) lightPosition.getX(), (float) lightPosition.getY(), (float) lightPosition.getZ(), 1f};
         glLightfv(GL_LIGHT0, GL_POSITION, pos);
-        glLightfv(GL_LIGHT0, GL_AMBIENT, colToFloat4(ambientColor));
-        glLightfv(GL_LIGHT0, GL_DIFFUSE, colToFloat4(diffuseColor));
-        glLightfv(GL_LIGHT0, GL_SPECULAR, colToFloat4(specularColor));
 
-        glLightModelfv(GL_LIGHT_MODEL_AMBIENT, colToFloat4(new Col(0.05, 0.05, 0.15)));
+        glLightfv(GL_LIGHT0, GL_AMBIENT, colToFloat(ambientColor));
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, colToFloatScaled(diffuseColor, intensity));
+        glLightfv(GL_LIGHT0, GL_SPECULAR, colToFloatScaled(specularColor, intensity));
+
+        glLightModelfv(GL_LIGHT_MODEL_AMBIENT, colToFloat(new Col(0.05, 0.05, 0.15)));
     }
 
-    private float[] colToFloat4(Col c) {
+    private float[] colToFloat(Col c) {
         return new float[]{(float) c.getR(), (float) c.getG(), (float) c.getB(), (float) c.getA()};
+    }
+
+    private float[] colToFloatScaled(Col c, float scale) {
+        return new float[]{
+                (float) (c.getR() * scale),
+                (float) (c.getG() * scale),
+                (float) (c.getB() * scale),
+                (float) c.getA()
+        };
     }
 
     public void setLightPosition(Vec3D position) {
